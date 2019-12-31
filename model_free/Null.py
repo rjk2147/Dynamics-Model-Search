d2fc03b89c6a40c793825f7be0b764bbb5b6865e
Robert Kwiatkowski <rjk2147@columbia.edu>
2019-12-31 03:09:17 +0000

    Added null agent

--- model_free/Null.py
+++ model_free/Null.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 class NullReplay(object):
     def __init__(self, state_dim, action_dim, max_size=int(1e6)):
         pass
@@ -6,15 +7,22 @@ class NullReplay(object):
     def add(self, state, action, next_state, reward, done):
         pass
 
+    def __len__(self):
+        return 0
+
 class NullAgent:
     def __init__(self, state_dim, act_dim):
         print('Null agent chosen')
         self.act_dim = act_dim
+        self.steps = 0
 
     def act(self, obs):
         action = torch.randn((obs.shape[0], self.act_dim))
-        return action
+        return action.to(obs.device)
 
     def value(self, obs, act, new_obs):
         r = new_obs[:,0].cpu().detach().numpy()
-        return torch.zeros_like(r)
+        return np.zeros_like(r)
+
+    def save(self, filename):
+        pass

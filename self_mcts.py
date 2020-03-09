normalization dimension fixed

self_mcts.py
@@ -111,7 +111,7 @@ class Agent:
         #appending r to new_obs
         new_obs = (np.append(new_obs[0], rew), new_obs[1])
         #until -1. Since the last one is for reward
-        self.x_seq.append(obs[0] / self.model.state_mul_const)
+        self.x_seq.append(obs[0] / self.model.state_mul_const[:-1])
         self.a_seq.append(act / self.act_mul_const)
         self.y_seq.append(new_obs[0] / self.model.state_mul_const)
         if len(self.x_seq) == self.seq_len:
@@ -119,7 +119,8 @@ class Agent:
         if len(self.model_replay) >= self.batch_size:
             data = random.sample(self.model_replay, self.batch_size)
 
-            obs_dist = np.array([step[0][0] for step in data])
+#tell robert that y was float64
+            obs_dist = np.array([step[2][0] for step in data]).astype(np.float32)
             obs_mean = np.mean(obs_dist, 0)
             obs_std = np.std(obs_dist, 0)
 
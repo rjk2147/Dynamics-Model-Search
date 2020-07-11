820bb195302bd3a3cd355629cfbe5607edc59644
YuZhan69 <yz3708@columbia.edu>
2020-07-11 01:34:32 -0400

    fixed Piecewise arguments

--- dynamics_model_search/model_free/DQN.py
+++ dynamics_model_search/model_free/DQN.py
@@ -352,9 +352,9 @@ class DQN:
     def __init__(self,
         env,
         exploration=PiecewiseSchedule([
-            (0,        1.0),
+            (0,   1.0),
             (1e6, 0.1),
-            (2e6, 0.01)
+            (1e7, 0.01)
         ], outside_value=0.01),
         replay_buffer_size=90000,
         gamma=0.99,

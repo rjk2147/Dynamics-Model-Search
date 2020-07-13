239fd48ad95b9ce37741c65083d7f10aa5930d61
YuZhan69 <yz3708@columbia.edu>
2020-07-12 20:56:02 -0400

    2 5 8

--- dynamics_model_search/model_free/DQN.py
+++ dynamics_model_search/model_free/DQN.py
@@ -353,9 +353,9 @@ class DQN:
         env,
         exploration=PiecewiseSchedule([
             (0,   1.0),
-            (1e6, 0.1),
-            (3e6, 0.01),
-            (6e6, 0.001),
+            (2e6, 0.1),
+            (5e6, 0.01),
+            (8e6, 0.001),
         ], outside_value=0.001),
         replay_buffer_size=90000,
         gamma=0.99,
